f37cad4d5655758107800a47933a5eacaba11a11
BrokenV0id <esky2403@gmail.com>
2026-02-26 21:22:38 +0000

    Update esp_chat.ino

--- code/esp_chat.ino
+++ code/esp_chat.ino
@@ -6,7 +6,7 @@
 #define CHANNEL 1
 String name = "device1"; // name of device
 
-esp_now_peer_info_t slave;
+esp_now_peer_info_t peer;
 
 // ============ FUNCTIONS ============
 void setup() {
@@ -16,10 +16,10 @@ void setup() {
   delay(1000); // give serial time to establish connection
   Serial.println("Device MAC: " + WiFi.macAddress());
 
-  uint8_t slaveAddress[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF}; // broadcast MAC address
-  memcpy(slave.peer_addr, slaveAddress, 6);
-  slave.channel = CHANNEL;
-  slave.encrypt = false;
+  uint8_t peerAddress[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF}; // broadcast MAC address
+  memcpy(peer.peer_addr, peerAddress, 6);
+  peer.channel = CHANNEL;
+  peer.encrypt = false;
 
   if (esp_now_init() != ESP_OK) {
     Serial.println("Failed to init ESP-NOW.");
@@ -28,7 +28,7 @@ void setup() {
 
   esp_now_register_send_cb(onDataSent);
   esp_now_register_recv_cb(OnDataRecv);
-  if (esp_now_add_peer(&slave) != ESP_OK) {
+  if (esp_now_add_peer(&peer) != ESP_OK) {
     Serial.println("Failed to add peer.");
     return;
   }
@@ -38,7 +38,7 @@ void loop() {
   if (Serial.available()) {
     String text = Serial.readStringUntil('\n');
     String message = name + ": " + text;
-    esp_now_send(slave.peer_addr, (uint8_t*)message.c_str(), message.length());
+    esp_now_send(peer.peer_addr, (uint8_t*)message.c_str(), message.length());
   }
 }
 
